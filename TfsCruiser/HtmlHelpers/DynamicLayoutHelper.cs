41c6a477821a10f3d01605af6639da2bb44b7912
Steve Fenton <github@stevefenton.co.uk>
2015-12-04 16:00:49 +0000

    Layout improvements.

---
+++ TfsCruiser/HtmlHelpers/DynamicLayoutHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace System.Web.Mvc.Html
+{
+    public static class DynamicLayoutHelper
+    {
+        public static string TileHeight(this HtmlHelper htmlHelper, int tileCount, int columns, decimal gapPercentage = 0, int precision = 6)
+        {
+            var tilesPerColumn = Math.Ceiling((decimal)tileCount / columns);
+            var tileHeight = Math.Round(((100 / tilesPerColumn) - gapPercentage), precision);
+            return tileHeight.ToString();
+        }
+
+        public static string MiniTileWidth(this HtmlHelper htmlHelper, int tileCount, int precision = 6)
+        {
+            var tileWidth = Math.Round((decimal)(100 / (tileCount + 1)), 6);
+            return tileWidth.ToString();
+        }
+    }
+}
---
+++ TfsCruiser/HtmlHelpers/StateMapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace System.Web.Mvc.Html
+{
+    public static class StateMapper
+    {
+        public static string MapState(this HtmlHelper htmlHelper, string state)
+        {
+            switch (state.ToLowerInvariant())
+            {
+                case "succeeded":
+                    return "Succeeded";
+                case "ontrack":
+                    return "OnTrack";
+                case "partiallysucceeded":
+                    return "PartiallySucceeded";
+                case "stopped":
+                    return "Stopped";
+                case "failed":
+                default:
+                    return "Failed";
+            }
+        }
+    }
+}
